07f395710c96b44c133d48b8f45d82c1784ee58e
Bartosz Romanowski <bartosz.romanowski2@gmail.com>
2018-12-28 12:13:38 +0100

    first commit

---
+++ src/main/java/com/batrom/tw/TomaszWebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.batrom.tw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomaszWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomaszWebsiteApplication.class, args);
+	}
+
+}
+
